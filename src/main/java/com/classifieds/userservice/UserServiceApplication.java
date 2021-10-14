73b9aaee24411b891d0e66aab1b8ef62c6afe9a9
Raghava <>
2021-10-14 16:11:27 +0530

    added all files

--- src/main/java/com/classifieds/userservice/UserServiceApplication.java
+++ src/main/java/com/classifieds/userservice/UserServiceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-			SpringApplication.run(UserServiceApplication.class, args);
+		System.out.println("test build pipeline");
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
